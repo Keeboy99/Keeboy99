19dfc5477b4a94ce1ef07d1d51f9500f68763ae1
Keeboy99 <67620144+Keeboy99@users.noreply.github.com>
2023-09-05 04:19:05 +1200

    shaudoihsaio

--- config.js
+++
@@ -1,5 +0,0 @@
-window.__CONFIG__ = {
-  // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "https://subdomain.keeboy.workers.dev",
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWNhOWMwOTUzZTA4Y2YxNjRkZDJhODFlMjUyZDBmMyIsInN1YiI6IjY0ZjVmZjc4MTIxOTdlMDBmZWE5NjM5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QpsAbymuE0p9MqH2jV02hvZN0nUVww_xM_MIjcYJuw4"
-};
